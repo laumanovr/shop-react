798bf717b6331796164f535b71bbb8431021551f
Ramazan <laumanov.r@gmail.com>
2019-07-04 17:57:53 +0600

    create car component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { connect } from 'react-redux';
+import { getAllCars, updateInfo } from './store/actions/index';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {cars: []};
+    }
+
+    componentWillMount() {
+        this.props.dispatch(getAllCars());
+    }
+
+    componentDidMount() {
+        document.addEventListener('DOMContentLoaded', () => {
+            this.setState({cars: this.props.cars});
+        });
+
+        setInterval(() => {
+            this.props.dispatch(updateInfo());
+            this.setState({cars: this.props.cars});
+        }, 3000)
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h2 className="title">Cars shop</h2>
+                <div className="cars">
+                    {this.state.cars.map(car =>
+                        <div className="car" key={car.id}>
+                            <div><img src={car.image} alt=""/></div>
+                            <p><b>Name: </b>{car.name}</p>
+                            <p><b>Year: </b>{car.year}</p>
+                            <p><b>Price: </b>{car.price}$</p>
+                        </div>
+                    )}
+                </div>
+            </div>
+
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+    return {
+        cars: state.allCars
+    };
+};
+
+export default connect(mapStateToProps)(App);
